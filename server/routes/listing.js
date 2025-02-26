4577873be1573affd07884de79caaef1e2a021e4
Noura Charrak <charrak.nora95@gmail.com>
2025-02-26 15:26:27 +0100

    modif upload 4

--- server/routes/listing.js
+++ server/routes/listing.js
@@ -9,7 +9,7 @@ const Listing = require("../models/Listing");
 const User = require("../models/user");
 
 // Configuration for file upload
-const uploadDir = path.join(dirname, './uploads');
+const uploadDir = path.join(__dirname, '../uploads');
 
 // Check if upload directory exists, create if not
 if (!fs.existsSync(uploadDir)) {
