25e2efc75ce05d687e0e5010dcc62021ff0f2685
Noura Charrak <charrak.nora95@gmail.com>
2025-02-27 21:04:34 +0100

    listing card 2

--- client/src/componenets/ListingCard.jsx
+++ client/src/componenets/ListingCard.jsx
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/listingCard.scss";
-import {
-  ArrowForwardIos,
-  ArrowBackIosNew,
-  Favorite,
-} from "@mui/icons-material";
+import { ArrowForwardIos, ArrowBackIosNew, Favorite } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
-import { setWishList } from "../redux/state";
-import URL from "../constants/api";
+import {setWishList} from '../redux/state'
+import URL from "../constants/api"
 
 const ListingCard = ({
   listingId,
@@ -43,21 +39,20 @@ const ListingCard = ({
   const dispatch = useDispatch();
 
   //  add the wishList
-  const user = useSelector((state) => state.user);
+  const user = useSelector((state) => state.user)
   const wishList = user?.wishList || [];
-  const isLiked = wishList.find((item) => item?._id === listingId);
-  const patchWishList = async () => {
+  const isLiked = wishList.find((item) => item?._id === listingId)
+  const patchWishList = async ()=> {
     if (user?._id !== creator._id) {
-      const response = await fetch(
-        `${URL.FETCH_USERS}/${user?._id}/${listingId}`,
-        { method: "PATCH", header: { "Content-Type": "application/jason" } }
-      );
-      const data = await response.json();
-      dispatch(setWishList(data.wishList));
-    } else {
-      return;
-    }
-  };
+    const response = await fetch(`${URL.FETCH_USERS}/${user?._id}/${listingId}`, 
+      {method: 'PATCH',
+       header: {'Content-Type': 'application/jason'} 
+      }
+    );
+    const data = await response.json()
+    dispatch(setWishList(data.wishList))
+  } else {return }
+  }
 
   return (
     <div className="listing-card">
@@ -73,27 +68,32 @@ const ListingCard = ({
             style={{ transform: `translateX(-${currentIndex * 100}%)` }}
           >
             {listingPhotosPaths && listingPhotosPaths.length > 0 ? (
-              <div className="slider-container">
-                <div
-                  className="slider"
-                  style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-                >
-                  {listingPhotosPaths.map((ListingPhoto, index) => (
-                    <div key={index} className="slide">
-                      <img
-                        src={`${URL.BACK_LINK}/${ListingPhoto}`}
-                        alt={`ListingPhoto ${index}`}
+              listingPhotosPaths.map((photo, index) => (
+                <div key={index} className="slide">
+                 <img
+                        src={`${URL.BACK_LINK}/${photo}`}
+                        alt="listingPhoto"
                       />
-                    </div>
-                  ))}
+                  <div
+                    className="prev-button"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      goToPrevSlide(e);
+                    }}
+                  >
+                    <ArrowBackIosNew sx={{ fontSize: "15px" }} />
+                  </div>
+                  <div
+                    className="next-button"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      goToNextSlide(e);
+                    }}
+                  >
+                    <ArrowForwardIos sx={{ fontSize: "15px" }} />
+                  </div>
                 </div>
-                <div className="prev-button" onClick={goToPrevSlide}>
-                  <ArrowBackIosNew sx={{ fontSize: "15px" }} />
-                </div>
-                <div className="next-button" onClick={goToNextSlide}>
-                  <ArrowForwardIos sx={{ fontSize: "15px" }} />
-                </div>
-              </div>
+              ))
             ) : (
               <div className="no-photos">
                 No photos available or undefined path.
@@ -123,19 +123,15 @@ const ListingCard = ({
               </p>
             </>
           )}
-          <button
-            className="favorite"
-            onClick={(e) => {
-              e.stopPropagation();
-              patchWishList();
-            }}
-            disabled={!user}
-          >
+          <button className="favorite" onClick={(e) => {
+            e.stopPropagation();
+            patchWishList()}}
+            disabled={!user}>
             {isLiked ? (
-              <Favorite sx={{ color: "red" }} />
-            ) : (
-              <Favorite sx={{ color: "white" }} />
-            )}
+              <Favorite sx={{color: "red"}}/> 
+            ): (
+                <Favorite sx={{color: "white"}}/>
+              )}
           </button>
         </div>
       </div>
