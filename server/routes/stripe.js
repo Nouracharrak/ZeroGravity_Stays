8d2f71bd68cb0b909b6a363d21943efb130adb75
Noura Charrak <charrak.nora95@gmail.com>
2025-03-06 02:10:27 +0100

    admin dashboard 10

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { createPaymentIntent, handlePaymentSuccess } = require("../controllers/stripeController");
-const { verifyToken } = require("../middleware/auth");  // Assure-toi d'importer ton middleware
+// const { verifyToken } = require("../middleware/auth");  // Assure-toi d'importer ton middleware
 
 // Route pour créer un Payment Intent (avec vérification du token)
-router.post("/create-payment-intent", verifyToken, createPaymentIntent);
+router.post("/create-payment-intent", createPaymentIntent);
 
 // Route pour gérer la réussite du paiement (avec vérification du token)
-router.post("/payment-success", verifyToken, handlePaymentSuccess);
+router.post("/payment-success", handlePaymentSuccess);
 
 module.exports = router;
 
